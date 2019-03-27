a0077cf879afa103115cbee5db1714cf1c4c713b
Rodrigo Yoshida <rodrigoymy@gmail.com>
2019-03-27 17:10:04 -0300

    Enabling Babel runtime to fix the bug on build file

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 "use strict";
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
 function asyncForEach(_x, _x2) {
   return _asyncForEach.apply(this, arguments);
 }
 
 function _asyncForEach() {
-  _asyncForEach = _asyncToGenerator(
+  _asyncForEach = (0, _asyncToGenerator2.default)(
   /*#__PURE__*/
-  regeneratorRuntime.mark(function _callee(array, callback) {
+  _regenerator.default.mark(function _callee(array, callback) {
     var index;
-    return regeneratorRuntime.wrap(function _callee$(_context) {
+    return _regenerator.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
